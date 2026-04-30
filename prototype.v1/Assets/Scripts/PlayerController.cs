720026a629ef10a6b2dd1f3c3ee47c36b42b7515
Nông Thế Hưng <nongthehung1177@gmail.com>
2026-04-30 08:30:26 +0700

    fn1.1

---
+++ prototype.v1/Assets/Scripts/PlayerController.cs
@@ -0,0 +1,106 @@
+﻿using UnityEngine;
+using UnityEngine.InputSystem;
+[RequireComponent(typeof(Rigidbody2D))]
+public class PlayerController : MonoBehaviour
+{
+    public float walkSpeed = 5f;
+    public float runSpeed = 8f;
+    Vector2 moveInput;
+    public float CurrentMoveSpeed { get
+        {
+            if (IsMoving)
+            {
+                if (IsRunning)
+                {
+                    return runSpeed;
+                } else
+                {
+                    return walkSpeed;
+                }
+            }
+            else
+            {
+                return 0;
+            }
+        }
+    }
+    [SerializeField]
+    private bool _isMoving = false;
+    public bool IsMoving { get 
+        {
+            return _isMoving;
+        }
+        private set
+        {
+            _isMoving = value;
+            animator.SetBool("isMoving", value);
+        } 
+    }
+    [SerializeField]
+    private bool _isRunning = false;
+    public bool IsRunning { get 
+        {
+            return _isRunning;
+        }
+        private set
+        {
+            _isRunning = value;
+            animator.SetBool("isRunning", value);
+        }
+    }
+    
+    public bool _isFacingRight = true;
+    public bool IsFacingRight { get { return _isFacingRight; } private set {
+             if (_isFacingRight != value)
+                {
+                transform.localScale *= new Vector2(-1, 1);
+                }
+             _isFacingRight = value;
+
+
+        } }
+
+    Rigidbody2D rb;
+    Animator animator;
+    private void Awake()
+    {
+        rb = GetComponent<Rigidbody2D>();
+        animator = GetComponent<Animator>();
+    }
+    private void FixedUpdate()
+    {
+        rb.linearVelocity = new Vector2(moveInput.x * CurrentMoveSpeed, rb.linearVelocity.y);
+    }
+    public void OnMove(InputAction.CallbackContext context)
+    {
+        moveInput = context.ReadValue<Vector2>();
+        IsMoving = moveInput != Vector2.zero;
+        SetFacingDirection(moveInput);
+    }
+    private void SetFacingDirection(Vector2 moveInput)
+    {
+        if (moveInput.x > 0 && !IsFacingRight)
+        {
+            // Đang đi sang phải VÀ đang KHÔNG nhìn sang phải -> Lật sang phải
+            IsFacingRight = true;
+        }
+        else if (moveInput.x < 0 && IsFacingRight)
+        {
+            // Đang đi sang trái VÀ ĐANG nhìn sang phải -> Lật sang trái
+            IsFacingRight = false;
+        }
+    }
+    public void OnRun(InputAction.CallbackContext context)
+    {
+        if (context.started)
+        {
+            IsRunning = true;
+            
+        }
+        else if (context.canceled)
+        {
+            IsRunning = false;
+            
+        }
+    }
+}
